692945b5094fa82ebb614f86303d5a6dda5a3e0f
HNH12 <62258989+HNH12@users.noreply.github.com>
2021-04-03 22:46:28 +0300

    1, 4 задания

--- Третья лаба/scripts/tasks.js
+++ Третья лаба/scripts/tasks.js
@@ -1,13 +1,44 @@
-document.getElementsByClassName('item').forEach(addEventListener("click", function(e) {
+let firstTask = function() {
+	let currentDate = new Date();
+	let taskDiv = document.querySelector("div.task-item.first");
+	let newElement = document.createElement("p");
+	let dateRightFormat = currentDate.toLocaleDateString();
+	newElement.appendChild(document.createTextNode(dateRightFormat));
+	taskDiv.appendChild(newElement);
+};
+
+
+let secondTask = function() {
+
+};
+
+
+let fourthTask = function(elem) {
+	setTimeout(function() {
+		elem.target.firstChild.classList.toggle('active');
+	}, 100);
+};
+
+
+firstTask();
+
+document.getElementsByClassName('item').forEach(
+	addEventListener("click", function(e) {
 	e.target.classList.toggle('active');
 
 	(function() {
 		nextSibling = e.target.nextElementSibling;
-		nextSibling.classList.toggle('active');
 		if(nextSibling.style.maxHeight) {
 			nextSibling.style.maxHeight = null;
 		} else {
-			nextSibling.style.maxHeight = nextSibling.scrollHeigth + "px";
+			nextSibling.style.maxHeight = nextSibling.scrollHeight + "px";
 		}
+		nextSibling.classList.toggle('active');
 	})();
+}));
+
+
+document.getElementsByClassName('container').forEach(
+	addEventListener("click", function(e) {
+	fourthTask(e.target);
 }))
