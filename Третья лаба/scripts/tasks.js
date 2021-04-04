de332c8f2a2775de28c3a023ee51ccf25b1da99a
HNH12 <62258989+HNH12@users.noreply.github.com>
2021-04-04 16:23:17 +0300

    6, 7, 8 задания

--- Третья лаба/scripts/tasks.js
+++ Третья лаба/scripts/tasks.js
@@ -13,6 +13,17 @@ let secondTask = function() {
 };
 
 
+let correctHeight = function(e) {
+	nextSibling = e.target.nextElementSibling;
+	if(nextSibling.style.maxHeight) {
+		nextSibling.style.maxHeight = null;
+	} else {
+		nextSibling.style.maxHeight = nextSibling.scrollHeight + "px";
+	}
+	nextSibling.classList.toggle('active');
+};
+
+
 let fourthTask = function(elem) {
 	setTimeout(function() {
 		elem.target.firstChild.classList.toggle('active');
@@ -20,37 +31,56 @@ let fourthTask = function(elem) {
 };
 
 
+let sixthTask = function() {
+	let table = document.querySelectorAll('table.table-task-sixth td');
+	for(let i = 0; i < table.length; i++) {
+		table[i].addEventListener('dblclick', function(e) {
+			let currentImg = e.target;
+			let siblingImg;
+			if(currentImg.parentNode.nextElementSibling !== null) {
+				siblingImg = currentImg.parentNode.nextElementSibling.firstElementChild;
+			}
+			else {
+				siblingImg = currentImg.parentNode.previousElementSibling.firstElementChild
+			}
+			currentImg.src = siblingImg.src;
+		});
+	}
+};
+
+
+let seventhTask = function() {
+	document.querySelector("div.sweety-title").addEventListener('click', function(e) {
+		console.log(e.target);
+	});
+};
+
+
 firstTask();
+sixthTask();
+seventhTask();
+
 
 document.getElementsByClassName('item').forEach(
 	addEventListener("click", function(e) {
 	e.target.classList.toggle('active');
 
-	if(e.target.classList.contains('fifth')) {
-		if(e.target.classList.contains('active')) {
-			setTimeout(function() {
-				while(true) {
-					let text = prompt('Введите текст блока (Esc для отмены)');
-					if(text == null) {
-						break;
-					}
-					let newElement = document.createElement("div");
-					newElement.appendChild(text);
-					e.target.nextSibling.firstChild.appendChild(newElement);
+	if(e.target.classList.contains('fifth') && e.target.classList.contains('active')) {
+		setTimeout(function() {
+			while(true) {
+				let text = prompt('Введите текст блока (Esc для отмены)');
+				if(text == null) {
+					break;
 				}
-			}, 250);
-		}
+				let newElement = document.createElement("div");
+				newElement.innerHTML = text;
+				e.target.nextElementSibling.firstElementChild.appendChild(newElement);
+			}
+		correctHeight(e);
+		}, 250);
+	} else {
+		correctHeight(e);
 	}
-
-	(function() {
-		nextSibling = e.target.nextElementSibling;
-		if(nextSibling.style.maxHeight) {
-			nextSibling.style.maxHeight = null;
-		} else {
-			nextSibling.style.maxHeight = nextSibling.scrollHeight + "px";
-		}
-		nextSibling.classList.toggle('active');
-	})();
 }));
 
 
