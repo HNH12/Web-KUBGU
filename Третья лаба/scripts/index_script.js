fa1a06741a2d5c2ea9f200660f3ba6c883351d05
HNH12 <62258989+HNH12@users.noreply.github.com>
2021-04-04 23:30:50 +0300

    2 и 9 задания

--- Третья лаба/scripts/index_script.js
+++ Третья лаба/scripts/index_script.js
@@ -1,3 +1,8 @@
 document.getElementById("first").addEventListener("click", (e) => {
 	e.target.style.background = 'none';
-})
+});
+
+
+document.getElementById('fourth').addEventListener('click', () => {
+	document.location = '../tasks/tasks.html';
+});
--- Третья лаба/scripts/tasks.js
+++ Третья лаба/scripts/tasks.js
@@ -8,8 +8,65 @@ let firstTask = function() {
 };
 
 
-let secondTask = function() {
+let calendar = function() {
+	let currentDate = new Date();
+    let lastDayMonth = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0).getDate();
+    let nameLastDayMonth = new Date(currentDate.getFullYear(), currentDate.getMonth(), lastDayMonth).getDay();
+    let nameFirstDayMonth = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1).getDay();
+    let calendar = '<tr>';
+    let month = ["Январь","Февраль","Март","Апрель","Май","Июнь","Июль","Август","Сентябрь","Октябрь","Ноябрь","Декабрь"];
+    let days = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'];
 
+    for(let i = 0; i < days.length; i++) {
+    	if(i > 4) {
+    		calendar += '<td class="weekend">' + days[i];
+    	} else {
+    		calendar += '<td>' + days[i];
+    	}
+    }
+    calendar += '<tr>';
+
+	if (nameFirstDayMonth != 0) {
+	  for(let i = 1; i < nameFirstDayMonth; i++) 
+	  	calendar += '<td>';
+	} else {
+	  for(let i = 0; i < 6; i++) 
+	  	calendar += '<td>';
+	}
+
+	for(let i = 1; i <= lastDayMonth; i++) {
+	  if (i != currentDate.getDate()) {
+	  	if(new Date(currentDate.getFullYear(), currentDate.getMonth(), i).getDay() == 0 || 
+	  		new Date(currentDate.getFullYear(), currentDate.getMonth(), i).getDay() == 6)
+	    	calendar += '<td class="weekend">' + i;
+	    else
+	    	calendar += '<td>' + i;
+	  } else {
+	    calendar += '<td id="today">' + i;
+	  }
+
+	  if (new Date(currentDate.getFullYear(), currentDate.getMonth(), i).getDay() == 0)
+	    calendar += '<tr>';
+	}
+
+	if (nameLastDayMonth != 0) {
+  		for(var  i = nameLastDayMonth; i < 7; i++) calendar += '<td>';
+	}
+
+	let fullDate = document.createElement("div");
+	let currentMonth = document.createElement("p");
+	let currentYear = document.createElement("p");
+
+	currentMonth.innerHTML = month[currentDate.getMonth()];
+	currentYear.innerHTML = currentDate.getFullYear();
+
+	fullDate.appendChild(currentMonth);
+	fullDate.appendChild(currentYear);
+	fullDate.classList.add('full-date');
+
+	document.querySelector('.task-item.second').prepend(fullDate);
+
+	document.querySelector('.task-item.second table').innerHTML = calendar;
 };
 
 
@@ -101,60 +158,69 @@ let seventhTask = function() {
 };
 
 
+let setListenersToImg = function() {
+	let arrBlocks = document.getElementsByClassName('container');
+	for(let i = 0; i< arrBlocks.length; i++) {
+		arrBlocks[i].addEventListener("click", function(e) {
+			fourthTask(e.target);
+		});
+	}
+
+	document.querySelector('.task-item.ninth img').addEventListener('mouseover', (e) => {
+		e.target.style.opacity = '0';
+	});
+
+	document.querySelector('.task-item.ninth img').addEventListener('mouseout', (e) => {
+		e.target.style.opacity = '1';
+	});
+};
+
+
+let prepareLoadPage = function() {
+	let arrItem = document.getElementsByClassName('item');
+	for(let i = 0; i < arrItem.length; i++) {
+		arrItem[i].addEventListener("click", function(e) {
+			e.target.classList.toggle('active');
+
+			if(e.target.classList.contains('fifth') && e.target.classList.contains('active')) {
+				setTimeout(function() {
+					while(true) {
+						let text = prompt('Введите текст блока (Esc для отмены)');
+						if(text == null) {
+							break;
+						}
+						let newElement = document.createElement("div");
+						newElement.innerHTML = text;
+						e.target.nextElementSibling.firstElementChild.appendChild(newElement);
+					}
+				correctHeight(e.target);
+				e.target.nextElementSibling.classList.toggle('active');
+				}, 250);
+			} else {
+				if(e.target.classList.contains('eighth') && e.target.classList.contains('active')) {
+					correctHeight(e.target);
+					e.target.nextElementSibling.classList.toggle('active');
+					setTimeout(function() {
+						e.target.nextElementSibling.firstElementChild.firstElementChild.style.opacity = '1';
+					}, 500);
+				}
+				else {
+					if(e.target.classList.contains('eighth') && !e.target.classList.contains('active')) {
+						e.target.nextElementSibling.firstElementChild.firstElementChild.style.opacity = '0'
+					}
+					correctHeight(e.target);
+					e.target.nextElementSibling.classList.toggle('active');
+				}
+			}
+		});
+	}
+
+	setListenersToImg();
+};
+
+
+calendar();
 firstTask();
 sixthTask();
 seventhTask();
-
-let arrItem = document.getElementsByClassName('item');
-for(let i = 0; i < arrItem.length; i++) {
-	arrItem[i].addEventListener("click", function(e) {
-		e.target.classList.toggle('active');
-
-		if(e.target.classList.contains('fifth') && e.target.classList.contains('active')) {
-			setTimeout(function() {
-				while(true) {
-					let text = prompt('Введите текст блока (Esc для отмены)');
-					if(text == null) {
-						break;
-					}
-					let newElement = document.createElement("div");
-					newElement.innerHTML = text;
-					e.target.nextElementSibling.firstElementChild.appendChild(newElement);
-				}
-			correctHeight(e.target);
-			e.target.nextElementSibling.classList.toggle('active');
-			}, 250);
-		} else {
-			if(e.target.classList.contains('eighth') && e.target.classList.contains('active')) {
-				correctHeight(e.target);
-				e.target.nextElementSibling.classList.toggle('active');
-				setTimeout(function() {
-					e.target.nextElementSibling.firstElementChild.firstElementChild.style.opacity = '1';
-				}, 500);
-			}
-			else {
-				if(e.target.classList.contains('eighth') && !e.target.classList.contains('active')) {
-					e.target.nextElementSibling.firstElementChild.firstElementChild.style.opacity = '0'
-				}
-				correctHeight(e.target);
-				e.target.nextElementSibling.classList.toggle('active');
-			}
-		}
-	});
-}
-
-
-let arrBlocks = document.getElementsByClassName('container');
-for(let i = 0; i< arrBlocks.length; i++) {
-	arrBlocks[i].addEventListener("click", function(e) {
-		fourthTask(e.target);
-	});
-}
-
-document.querySelector('.task-item.ninth img').addEventListener('mouseover', (e) => {
-	e.target.style.opacity = '0';
-});
-
-document.querySelector('.task-item.ninth img').addEventListener('mouseout', (e) => {
-	e.target.style.opacity = '1';
-});
+prepareLoadPage();
