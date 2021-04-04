3a8170e0c1899b1263d6d469151aea7a3664b889
HNH12 <62258989+HNH12@users.noreply.github.com>
2021-04-04 18:31:54 +0300

    7 задание до конца

--- Третья лаба/scripts/tasks.js
+++ Третья лаба/scripts/tasks.js
@@ -14,19 +14,18 @@ let secondTask = function() {
 
 
 let correctHeight = function(e) {
-	nextSibling = e.target.nextElementSibling;
+	nextSibling = e.nextElementSibling;
 	if(nextSibling.style.maxHeight) {
 		nextSibling.style.maxHeight = null;
 	} else {
 		nextSibling.style.maxHeight = nextSibling.scrollHeight + "px";
 	}
-	nextSibling.classList.toggle('active');
 };
 
 
 let fourthTask = function(elem) {
 	setTimeout(function() {
-		elem.target.firstChild.classList.toggle('active');
+		elem.classList.toggle('active');
 	}, 100);
 };
 
@@ -49,10 +48,61 @@ let sixthTask = function() {
 };
 
 
+let getNextSiblings = function(elem) {
+	let arrSibling = [];
+	while(elem.nextElementSibling) {
+		arrSibling.push(elem.nextElementSibling);
+		elem = elem.nextElementSibling;
+	}
+	return arrSibling;
+};
+
+
+let liEventHandler = function (event) {
+	event.target.style.opacity = '0';
+	let nextSiblings = getNextSiblings(event.target);
+	let coordYDeletedItem = event.target.getBoundingClientRect().y;
+	let distanceNearests = nextSiblings.length == 0 ? undefined : nextSiblings[0].getBoundingClientRect().y - coordYDeletedItem;
+
+	for(let i = 0; i < nextSiblings.length; i++) {
+		nextSiblings[i].style.transition = 'transform 0.4s linear';
+		nextSiblings[i].style.transform = `translateY(${-distanceNearests}px)`;
+	}
+
+	setTimeout(function() {
+		event.target.style.display = 'none';
+		for(let i = 0; i < nextSiblings.length; i++) {
+			nextSiblings[i].style.transition = 'opacity 0.2s linear';
+			nextSiblings[i].style.transform = "";
+		}		
+		let parent = event.target.parentNode;
+		event.target.remove();
+		if(parent.children.length == 0) {
+			parent.innerHTML = 'Сладости закончились';
+		}
+	}, 420);
+};
+
+
 let seventhTask = function() {
 	document.querySelector("div.sweety-title").addEventListener('click', function(e) {
-		console.log(e.target);
-	});
+		e.target.classList.toggle('active');
+		correctHeight(e.target);
+		e.target.nextElementSibling.classList.toggle('active');
+		let mainSibling = e.target.parentNode.parentNode.previousElementSibling;
+		console.log(mainSibling, mainSibling.nextElementSibling);
+		mainSibling.nextElementSibling.style.maxHeight = mainSibling.nextElementSibling.scrollHeight + "px";
+	})
+
+	let arrLi = document.querySelector('ul.sweety-menu').children;
+	for(let i = 0; i < arrLi.length; i++) {
+		arrLi[i].addEventListener('click', liEventHandler, false);
+	}
+};
+
+
+let eighthTask = function() {
+	
 };
 
 
@@ -60,31 +110,36 @@ firstTask();
 sixthTask();
 seventhTask();
 
+let arrItem = document.getElementsByClassName('item');
+for(let i = 0; i < arrItem.length; i++) {
+	arrItem[i].addEventListener("click", function(e) {
+		e.target.classList.toggle('active');
 
-document.getElementsByClassName('item').forEach(
-	addEventListener("click", function(e) {
-	e.target.classList.toggle('active');
-
-	if(e.target.classList.contains('fifth') && e.target.classList.contains('active')) {
-		setTimeout(function() {
-			while(true) {
-				let text = prompt('Введите текст блока (Esc для отмены)');
-				if(text == null) {
-					break;
+		if(e.target.classList.contains('fifth') && e.target.classList.contains('active')) {
+			setTimeout(function() {
+				while(true) {
+					let text = prompt('Введите текст блока (Esc для отмены)');
+					if(text == null) {
+						break;
+					}
+					let newElement = document.createElement("div");
+					newElement.innerHTML = text;
+					e.target.nextElementSibling.firstElementChild.appendChild(newElement);
 				}
-				let newElement = document.createElement("div");
-				newElement.innerHTML = text;
-				e.target.nextElementSibling.firstElementChild.appendChild(newElement);
-			}
-		correctHeight(e);
-		}, 250);
-	} else {
-		correctHeight(e);
-	}
-}));
+			correctHeight(e.target);
+			e.target.nextElementSibling.classList.toggle('active');
+			}, 250);
+		} else {
+			correctHeight(e.target);
+			e.target.nextElementSibling.classList.toggle('active');
+		}
+	});
+}
 
 
-document.getElementsByClassName('container').forEach(
-	addEventListener("click", function(e) {
-	fourthTask(e.target);
-}));
+let arrBlocks = document.getElementsByClassName('container');
+for(let i = 0; i< arrBlocks.length; i++) {
+	arrBlocks[i].addEventListener("click", function(e) {
+		fourthTask(e.target);
+	});
+}
