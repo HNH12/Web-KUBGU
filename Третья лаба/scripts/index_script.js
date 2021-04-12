4a17ae0d023cb77e0425833565451c06baa28518
HNH12 <62258989+HNH12@users.noreply.github.com>
2021-04-12 13:31:24 +0300

    Окончательная версия с заданием и биографией

--- Третья лаба/scripts/index_script.js
+++ Третья лаба/scripts/index_script.js
@@ -5,3 +5,11 @@ document.getElementById('fourth').addEventListener('click', () => {
 document.getElementById('third').addEventListener('click', () => {
 	document.location.href = 'bio/bio.html';
 });
+
+document.getElementById('first').addEventListener('click', () => {
+	document.location.href = 'resume/res.html';
+});
+
+document.getElementById('second').addEventListener('click', () => {
+	document.location.href = 'bio/hobby.html';
+});
